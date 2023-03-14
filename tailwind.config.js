c8ff61ec6d92ababb9826854f0e3ee8c24a4eaaa
vic-147 <dmkorps@gmail.com>
2023-03-14 19:18:28 -0300

    nuevo curso

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./src/**/*{html,js,jsx}"],
+  content: [],
+  theme: {
+    colors: {
+      ...colors,
+    },
+  },
+};

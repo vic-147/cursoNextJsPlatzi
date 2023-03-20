e9244b85e790f630738bf04498fcf4ac7fc3faa6
vic-147 <dmkorps@gmail.com>
2023-03-20 14:22:24 -0300

    :rocket: preparando para deploy

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ['tailwindui.com', 'images.unsplash.com', 'loremflickr.com'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
